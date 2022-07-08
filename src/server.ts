4313d1dc0646d6c3374f3e5b9e8bb745cb98248c
Horlaitan15 <ajanihorlaitan@gmail.com>
2022-07-08 16:05:33 +0100

    Corrected TypeScript typing mistake.

--- src/server.ts
+++ src/server.ts
@@ -29,7 +29,7 @@ import {filterImageFromURL, deleteLocalFiles} from './util/util';
 
   /**************************************************************************** */
 
-app.get("/filteredimage/",async (req, res)=>{
+app.get("/filteredimage/",async (req: express.Request, res: express.Response)=>{
     let {image_url}: any = req.query;
     if( !image_url ) {
       return res.status(422)
